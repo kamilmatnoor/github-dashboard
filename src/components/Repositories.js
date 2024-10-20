893d22926725f27bc400dbf657d70759ba60c845
Muhamad Kamil Muhamad Noor <kamilmatnoor@gmail.com>
2024-10-20 11:54:51 +0800

    chore(Repositories):add fontawesome + update ui

--- src/components/Repositories.js
+++ src/components/Repositories.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState, useRef } from "react";
 import api from "../api/http-request";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 const Repositories = () => {
     const [items, setItems] = useState([]);
     const [pageNumber, setPageNumber] = useState(1);
     const observerRef = useRef();
 
-    const formatNumber = (num) => {
+    const formatStars = (num) => {
         if (num > 999999) {
             let r = num / 1000;
             return r.toFixed(2) + 'm';
@@ -50,7 +52,7 @@ const Repositories = () => {
         <div>
             <div className="container mx-auto mb-4 text-left">
                 <h3 className="text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-2xl dark:text-white">Trending Repositories</h3>
-                <hr class="w-full h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+                <hr className="w-full h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
             </div>
 
             <ul className="">
@@ -59,10 +61,18 @@ const Repositories = () => {
                         <div className="px-4 py-5 sm:px-6">
                             <div className="flex items-center justify-between">
                                 <h3 className="text-lg leading-6 font-medium text-gray-900">{item.full_name}</h3>
-                                <p className="mt-1 max-w-2xl text-sm text-gray-500">{formatNumber(item.stargazers_count)}</p>
                             </div>
                             <div className="mt-4 flex items-center text-left">
-                                <p className="text-sm font-medium text-gray-500">{item.description}</p>
+                                <p className="text-sm font-medium text-gray-500">{item.description ? item.description : 'Description is not applicable'}</p>
+                            </div>
+                            <div className="mt-4 flex items-center justify-between">
+                                <div className="flex items-center text-left">
+                                    <img src={item.owner.avatar_url} alt="Cheetah!" className="w-5" />
+                                    <span className="mx-1">{item.owner.login}</span>
+                                </div>
+                                <div className="flex items-center text-right">
+                                    <p className="mt-1 max-w-2xl text-sm text-gray-500"><FontAwesomeIcon icon={faStar} /> {formatStars(item.stargazers_count)}</p>
+                                </div>
                             </div>
                         </div>
                     </li>
