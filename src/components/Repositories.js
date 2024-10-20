b054237b43719493e9937ab3afe653a8ea1bd2f9
Muhamad Kamil Muhamad Noor <kamilmatnoor@gmail.com>
2024-10-20 09:16:26 +0800

    chore(style): implement tailwind css + implement parameter github

--- src/components/Repositories.js
+++ src/components/Repositories.js
@@ -13,9 +13,19 @@ const Repositories = () => {
     }, []);
     return (
         <div>
-            <ul>
+            <ul className="">
                 {items.map((item, index) => (
-                    <li key={index}>{item.full_name}</li>
+                    <li className="bg-white shadow overflow-hidden sm:rounded-md mx-auto mb-16" key={index}>
+                        <div className="px-4 py-5 sm:px-6">
+                            <div className="flex items-center justify-between">
+                                <h3 className="text-lg leading-6 font-medium text-gray-900">{item.full_name}</h3>
+                                <p className="mt-1 max-w-2xl text-sm text-gray-500">{item.stargazers_count}</p>
+                            </div>
+                            <div className="mt-4 flex items-center text-left">
+                                <p className="text-sm font-medium text-gray-500">{item.description}</p>
+                            </div>
+                        </div>
+                    </li>
                 ))}
             </ul>
         </div>
